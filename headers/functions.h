3114a3b83b4cc28956578da1ff69b3ede240eca3
sebpost2 <81270882+sebpost2@users.noreply.github.com>
2021-06-28 21:25:55 -0500

    Add files via upload

---
+++ headers/functions.h
@@ -0,0 +1,42 @@
+#include <NTL/ZZ.h>
+#include <string>
+#include <vector>
+#include <windows.h>
+
+using namespace NTL;
+using std::vector;
+using std::string;
+
+vector<ZZ> extMcdZZ(ZZ, ZZ);
+ZZ inversa(ZZ, ZZ);
+ZZ mcdZZ(ZZ, ZZ);
+int modulo(int ,int );
+ZZ moduloZZ(ZZ, ZZ);
+ZZ moduloZZ2(ZZ);
+ZZ powerZZ(ZZ, ZZ, ZZ);
+ZZ rtl(ZZ,ZZ);
+ZZ get_coordinate();
+ZZ CL(ZZ,ZZ,ZZ,ZZ);
+ZZ Getrand(ZZ);
+bool isPrime(ZZ);
+class blumblumshubRNG{
+public:
+    ZZ seed;
+    ZZ m;
+    blumblumshubRNG(time_t , ZZ );
+    ZZ next();
+};
+class coordinateRNG{
+public:
+    ZZ seed, nBits, a, c,m;
+    coordinateRNG(ZZ);
+    ZZ next();
+private:
+    ZZ get_coordinate();
+};
+ZZ getPrime(coordinateRNG &);
+string zToString(ZZ);
+vector <string> stringToVector(string, int);
+vector <string> stringToVectorIndex(string, int, string);
+string vectorToString(vector <string>);
+
---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <NTL/ZZ.h>
+#include <iostream>
+#include "./headers/RSA.h"
+#include "headers/functions.h"
+
+#include <vector>
+// g++ -g -O2 -std=c++11 -pthread -march=native "$string/$file" -o "$string/$output" -lntl -lgmp -lm
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    string alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ,.-( )abcdefghijklmnopqrstuvwxyz<>*1234567890";
+    cout<<"\nEmisor\n";RSA Emisor(128, alpha);
+    cout<<"\nReceptor\n";RSA Receptor(128, alpha);
+
+    string mensaje;
+    cout << "\nIngrese el mensaje a ser cifrado: ";
+    getline(cin, mensaje);
+    ZZ eb=Receptor.e;
+    ZZ Nb=Receptor.N;
+    //Emisor cifra
+    string mensajeCifrado = Emisor.cypher(mensaje,eb,Nb);
+    cout << "El mensajeCifrado es: " << mensajeCifrado << endl;
+    //Receptor decifra
+    string mensajeDecifrado = Receptor.decypher(mensajeCifrado);
+    cout << "El mensajeDecifrado es: " << mensajeDecifrado << endl;
+}
